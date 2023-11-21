b519fb11442b9940aace0b036c79c1958c9c535a
Leyla Shafiyeva <llshafi03@gmail.com>
2023-11-21 22:50:09 +0400

    Tag Index and Create

---
+++ Pronia/Areas/ProniaAdmin/Controllers/TagController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Pronia.DAL;
+using Pronia.Models;
+
+namespace Pronia.Areas.ProniaAdmin.Controllers
+{
+    [Area("ProniaAdmin")]
+    public class TagController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public TagController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var Tags = await _context.Tags.Include(t=>t.ProductTags).ToListAsync();
+            return View(Tags);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Tag tag)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            bool result = _context.Tags.Any(c => c.Name.ToLower().Trim() == tag.Name.ToLower().Trim());
+            if (result)
+            {
+                ModelState.AddModelError("Name", "This tag already exists");
+                return View();
+            }
+            await _context.Tags.AddAsync(tag);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+    }
+}
--- Pronia/Models/Tag.cs
+++ Pronia/Models/Tag.cs
@@ -1,8 +1,13 @@
-﻿namespace Pronia.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Pronia.Models
 {
     public class Tag
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        [MaxLength(25, ErrorMessage = "Name's max length is 25")]
         public string Name { get; set; }
         public List<ProductTag>? ProductTags { get; set; }
     }
