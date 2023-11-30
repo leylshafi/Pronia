f3d6c6205dc7b0a34ecf4b11e694b48314fbd56c
Leyla Shafiyeva <llshafi03@gmail.com>
2023-12-01 00:31:06 +0400

    View component done

---
+++ Pronia/ViewComponents/ProductViewComponent.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Pronia.DAL;
+using Pronia.Models;
+
+namespace Pronia.ViewComponents
+{
+    public class ProductViewComponent:ViewComponent
+    {
+        private readonly AppDbContext _context;
+
+        public ProductViewComponent(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int key=1)
+        {
+            List<Product> products;
+            switch (key)
+            {
+                case 1:
+                    products =await 
+                 _context.Products
+                 .OrderBy(p=>p.Name)
+                .Take(8)
+                .Include(p => p.ProductImages
+                .Where(pi => pi.IsPrimary != null))
+                .ToListAsync();
+                    break;
+                case 2:
+                    products = await
+                 _context.Products
+                 .OrderByDescending(p => p.Price)
+                .Take(8)
+                .Include(p => p.ProductImages
+                .Where(pi => pi.IsPrimary != null))
+                .ToListAsync();
+                    break;
+                case 3:
+                    products = await
+                _context.Products
+                .OrderByDescending(p => p.Id)
+               .Take(8)
+               .Include(p => p.ProductImages
+               .Where(pi => pi.IsPrimary != null))
+               .ToListAsync();
+                    break;
+                default:
+                    products = await
+                _context.Products
+               .Take(8)
+               .Include(p => p.ProductImages
+               .Where(pi => pi.IsPrimary != null))
+               .ToListAsync();
+                    break;
+            }
+            return View(products);
+        }
+    }
+}
